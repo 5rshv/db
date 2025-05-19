d181f8755ad45e2f8c8bcfd203d09b95c45aa283
5rshv <serishev.alesha@gmail.com>
2025-05-19 21:58:55 +0300

    commit DB SQL learning

--- src/main/java/skypro/hogwarts/repository/StudentRepository.java
+++ src/main/java/skypro/hogwarts/repository/StudentRepository.java
@@ -1,14 +1,26 @@
 package skypro.hogwarts.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import skypro.hogwarts.model.Student;
 
 import java.util.Collection;
+import java.util.List;
 
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Collection<Student> findStudentsByAgeBetween(int min, int max);
 
     Collection<Student> findStudentByNameIgnoreCase(String name);
+
+    @Query(value = "select count(id) from student", nativeQuery = true)
+    Collection<Integer>getStudentsId();
+
+    @Query(value = "select round(avg(age)) from student;", nativeQuery = true)
+    Collection<Integer> getStudentsAgeAverage();
+
+    @Query(value = "select * from student order by id desc limit 5;", nativeQuery = true)
+    Collection<Integer>getStudentsLastFive();
 }
 
